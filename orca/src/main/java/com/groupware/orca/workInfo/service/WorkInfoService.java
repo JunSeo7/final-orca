a42c75c6615e54208ef9820d0e4b488e341ca54e
LAPTOP-BSIG081T\saqw3 <wnstj1478@gmail.com>
2024-06-28 13:53:10 +0900

    [add] jsp 추가

--- orca/src/main/java/com/groupware/orca/workInfo/service/WorkInfoService.java
+++ orca/src/main/java/com/groupware/orca/workInfo/service/WorkInfoService.java
@@ -16,21 +16,24 @@ public class WorkInfoService {
 
     private final WorkInfoDao dao;
 
+    // 근무 정보 조회
     public List<WorkInfoVo> workList() {
-
         return dao.workList();
     }
 
+    // 출근
     public WorkInfoVo startWork(WorkInfoVo vo) {
         return dao.startWork(vo);
     }
 
+    // 퇴근
     public WorkInfoVo endWork(WorkInfoVo vo) {
         dao.endWork(vo);
         dao.overTimeWork(vo);
         return vo;
     }
 
+    // 연장근무
     public WorkInfoVo overTimeWork(WorkInfoVo vo) {
 
         String endTimeStr = vo.getEnd_time();  // end_time은 endWork 호출 후 vo에 업데이트된 값이라고 가정
@@ -38,18 +41,19 @@ public class WorkInfoService {
         LocalDateTime endTime = LocalDateTime.parse(endTimeStr, formatter);
         LocalDateTime standardTime = endTime.toLocalDate().atTime(LocalTime.of(18, 0));
 
-        // 퇴근 시간이 18:00을 초과했는지 확인합니다.
+        // 퇴근 시간 18:00 넘는지 체크.
         if (endTime.isAfter(standardTime)) {
-            // 초과된 시간을 계산합니다.
+            // 넘은 시간 계산.
             int overtimeHours = endTime.getHour() - standardTime.getHour();
             if (endTime.getMinute() > 0 || endTime.getSecond() > 0) {
                 overtimeHours += 1;  // 분이나 초가 있으면 시간 단위로 올림
             }
 
-            // 초과된 시간을 WorkInfoVo에 설정합니다.
             vo.setOvertime_work(overtimeHours);
-            dao.overTimeWork(vo);
+        } else {
+            vo.setOvertime_work(0);
         }
+        dao.overTimeWork(vo);
         return vo;
     }
 }
