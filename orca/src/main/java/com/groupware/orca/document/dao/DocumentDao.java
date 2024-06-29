f8622a8982fbdc44a042f053bc44806788d2d8e8
DESKTOP-8QC2TON\JIWONNA <raji1004@naver.com>
2024-06-29 14:23:16 +0900

    [add] - 결재 작성,목록 기능구현

--- orca/src/main/java/com/groupware/orca/document/dao/DocumentDao.java
+++ orca/src/main/java/com/groupware/orca/document/dao/DocumentDao.java
@@ -2,8 +2,10 @@ package com.groupware.orca.document.dao;
 
 import com.groupware.orca.document.mapper.DocumentMapper;
 import com.groupware.orca.document.vo.DocumentVo;
+import com.groupware.orca.document.vo.TemplateVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +15,19 @@ public class DocumentDao {
 
     private final DocumentMapper mapper;
 
+    // 결재 작성 화면
+    public List<TemplateVo> getTemplateList() {
+        return mapper.getTemplateList();
+    }
+    //템플릿 가져오기
+    public TemplateVo getTemplateContent(int templateNo) {
+        return mapper.getTemplateContent(templateNo);
+    }
+    // 결재 작성
+    @Transactional
+    public int writeDocument(DocumentVo vo) {
+        return mapper.writeDocument(vo);
+    }
     //전체목록
     public List<DocumentVo> getDocumentList() {
         return mapper.getDocumentList();
