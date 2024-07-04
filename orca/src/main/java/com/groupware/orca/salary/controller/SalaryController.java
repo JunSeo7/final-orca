56e9b2193662679d1926407a9d2ea131da93c261
HYEWON\chlgp <chlgp5717@naver.com>
2024-07-04 12:04:13 +0900

    [edit] salary 오류 수정 미완

--- orca/src/main/java/com/groupware/orca/salary/controller/SalaryController.java
+++ orca/src/main/java/com/groupware/orca/salary/controller/SalaryController.java
@@ -5,10 +5,9 @@
 //import com.groupware.orca.salary.vo.SalaryVo;
 //import com.groupware.orca.user.vo.UserVo;
 //import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
 //
 //@RestController
 //@RequiredArgsConstructor
@@ -17,12 +16,28 @@
 //
 //    private final SalaryService service;
 //
-//    //급여계산 입력
-//    @GetMapping("write")
-//    public double salaryInsert(@RequestParam("sal"+"holidayTime"+"overTime") UserVo vo, SalaryVo svo, RatesVo rvo, double holidayTime, double overTime){
-//        double result = service.salaryWrite(vo,svo,rvo,holidayTime,overTime);
-//        return result;
+//    @GetMapping
+//    public void aa(){
+//        System.out.println("SalaryController.aa");
+//    }
 //
+//    //급여계산 입력
+//    @PostMapping("write")
+//    public double salaryWrite(
+//            UserVo vo,
+//            SalaryVo svo,
+//            RatesVo rvo,
+//            @RequestParam (value = "holidayTime" , required = false) double holidayTime,
+//            @RequestParam (value = "overTime", required = false) double overTime,
+//            @RequestParam (value = "person",required = false) double person,
+//            @RequestParam (value = "position", required = false) double position,
+//            @RequestParam (value = "bonus", required = false) double bonus,
+//            @RequestParam(value = "meals", required = false) double meals){
+//        System.out.println("SalaryController.salaryWrite");
+//
+//        double result = service.salaryWrite(vo,svo,rvo,holidayTime,overTime,person,position,bonus,meals);
+//        return result;
+//                //TODO 화면에서 자녀수에 따른 공제내역 사진 띄우고 적게 하기, param은 다 금액으로 입력받게 하기
 //    }
 //
 //    //4대보험 입력
@@ -37,10 +52,22 @@
 //    //4대보험 수정
 //
 //    //목록조회
+//    @GetMapping("list")
+//    public List<SalaryVo> getSalaryList(){
+//
+//        System.out.println("SalaryController.getSalaryList");
+//
+//        List<SalaryVo> voList = service.getSalaryList();
+//
+//
+//        return voList;
+//    }
 //
 //    //상세조회
 //
-//    //검색
+//    //급여- 검색
+//
+//    //퇴직 - 검색
 //
 //    //퇴직금 지급
 //
--- orca/src/main/java/com/groupware/orca/salary/mapper/SalaryMapper.java
+++ orca/src/main/java/com/groupware/orca/salary/mapper/SalaryMapper.java
@@ -2,19 +2,45 @@
 //
 //import com.groupware.orca.salary.vo.RatesVo;
 //import com.groupware.orca.salary.vo.SalaryVo;
+//import com.groupware.orca.user.vo.UserVo;
 //import org.apache.ibatis.annotations.Insert;
 //import org.apache.ibatis.annotations.Mapper;
+//import org.apache.ibatis.annotations.Param;
+//import org.apache.ibatis.annotations.Select;
+//
+//import java.util.List;
 //
 //@Mapper
 //public interface SalaryMapper {
 //
 //    //급여계산 입력 쿼리문
-//    @Insert("INSERT INTO PAYROLL(PAYROLL_NO ,EMP_NO ,NATIONAL_PENSION ,HEALTH_INSURANCE ,LONG_CARE ,EMPLOYMENT_INSURANCE ,INCOME_TAX ,LOCAL_INCOME_TAX ,POSITION ,BONUS ,HOLIDAY ,OVERTIMEWORK ,MEALS ,PAYMENT_DATE) \n" +
-//            "VALUES(SEQ_PAYROLL_NO.NEXTVAL,#{empNo},?,?,?,?,?,?,?,?,?,?,?,?)")
-//    int salarywrite(SalaryVo vo);
+//    @Insert("INSERT INTO PAYROLL(PAYROLL_NO, EMP_NO, NATIONAL_PENSION ,HEALTH_INSURANCE ,LONG_CARE ,EMPLOYMENT_INSURANCE,INCOME_TAX ,LOCAL_INCOME_TAX ,POSITION ,BONUS ,HOLIDAY ,OVERTIMEWORK ,MEALS ,TOTAL_SALARY,PAYMENT_DATE)\n" +
+//            "SELECT \n" +
+//            "    SEQ_PAYROLL_NO.NEXTVAL\n" +
+//            "    , E.EMP_NO\n" +
+//            "    , (E.SALARY * R.PENSION_PERCENTAGE) AS RENSION\n" +
+//            "    , (E.SALARY * R.HEALTH_INSURANCE_PERCENTAGE) AS HEALTH\n" +
+//            "    , (E.SALARY * R.HEALTH_INSURANCE_PERCENTAGE) * LONG_CARE_PERCENTAGE AS LONG_CARE\n" +
+//            "    ,(E.SALARY*EMPLOYMENT_INSURANCE_PERCENTAGE) AS EMPLOYMENT\n" +
+//            "    ,#{svo.incomeTax}\n" +
+//            "    ,#{svo.localIncomeTax}\n" +
+//            "    ,#{svo.position}\n" +
+//            "    ,#{svo.bonus}\n" +
+//            "    ,#{svo.holiday}\n" +
+//            "    ,#{svo.overTimeWork}\n" +
+//            "    ,#{svo.meals}\n" +
+//            "    ,(E.SALARY - RENSION - HEALTH - LONG_CARE - EMPLOYMENT -#{svo.incomeTax}- #{svo.localIncomeTax} + #{svo.position} + #{svo.bonus} + #{svo.holiday} + #{svo.overTimeWork} + #{svo.meals})\n" +
+//            "    ,SYSDATE\n" +
+//            "FROM PERSONNEL_INFORMATION E, RATES R\n" +
+//            "WHERE E.EMP_NO = #{vo.empNo}")
+//    int salaryWrite(@Param("vo") UserVo vo,@Param("svo") SalaryVo svo, @Param("rvo") RatesVo rvo, double holidayTime, double overTime, double person, double position, double bonus, double meals);
+//
 //
 //    //4대보험 입력 쿼리문
-//    @Insert("INSERT INTO RATES(BASE_YEAR,PENSION_PERCENTAGE,HEALTH_INSURANCE_PERCENTAGE,LONG_CARE,EMPLOYMENT_INSURANCE_PERCENTAGE) \n" +
-//            "VALUES(?,?,?,?,?)")
+//    @Insert("INSERT INTO RATES(BASE_YEAR,PENSION_PERCENTAGE,HEALTH_INSURANCE_PERCENTAGE,LONG_CARE_PERCENTAGE,EMPLOYMENT_INSURANCE_PERCENTAGE,LOCAL_INCOME_TAX_PERSENTAGE) \n" +
+//            "VALUES(SYSDATE,#{pensionPercentage},#{healthPercentage},#{longCarePercentage},#{employmentPercentage},#{localIncomeTaxPercentage})")
 //    int ratesWrite(RatesVo vo);
+//
+//    @Select("SELECT * FROM SALARY")
+//    List<SalaryVo> getSalaryList();
 //}
