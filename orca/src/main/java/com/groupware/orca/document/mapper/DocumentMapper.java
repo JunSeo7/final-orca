89d8ef4ae52a75fa729ba85c63bb55beec9ea499
DESKTOP-8QC2TON\JIWONNA <raji1004@naver.com>
2024-07-04 22:25:05 +0900

    [edit] - 결재 쿼리수정

--- orca/src/main/java/com/groupware/orca/document/mapper/DocumentMapper.java
+++ orca/src/main/java/com/groupware/orca/document/mapper/DocumentMapper.java
@@ -21,11 +21,16 @@ public interface DocumentMapper {
     @Select("SELECT AT.APPR_LINE_NO, AT.APPR_LINE_NAME , AT.CREATED_DATE, DT.TEMPLATE_NO , DT.TITLE , DT.CONTENT ,DTC.CATEGORY_NO, DTC.NAME AS CATEGORY_NAME FROM APPR_LINE_TEMPLATE AT JOIN DOC_TEMPLATE DT ON AT.TEMPLATE_NO = DT.TEMPLATE_NO JOIN DOC_TEMPLATE_CATEGORY DTC ON DT.CATEGORY_NO = DTC.CATEGORY_NO WHERE AT.TEMPLATE_NO = #{templateNo} AND WRITER_NO IS NULL")
     ApprovalLineVo getTemplateApprLine(int templateNo);
     // 결재 작성 결재자 여러명 가져오기
-    @Select("SELECT AI.APPROVER_INFO_NO, AI.APPR_LINE_NO, AI.SEQ , AI.APPROVER_CLASSIFICATION_NO, PI.NAME approverName " +
-            ", D.DEPT_CODE, D.PARTNAME AS DEPT_NAME, P.NAME_OF_POSITION positionName FROM APPROVER_INFO AI " +
-            "JOIN PERSONNEL_INFORMATION PI ON AI.APPROVER_NO = PI.EMP_NO JOIN DEPARTMENT D ON D.DEPT_CODE = PI.DEPT_CODE " +
-            "JOIN POSITION P ON P.POSITION_CODE = PI.POSITION_CODE WHERE APPR_LINE_NO = #{apprLineNo} " +
-            "ORDER BY AI.SEQ")
+    @Select("""
+            SELECT AI.APPROVER_INFO_NO, AI.APPR_LINE_NO, AI.SEQ, AI.APPROVER_CLASSIFICATION_NO, PI.NAME approverName,
+            D.DEPT_CODE, D.PARTNAME AS DEPT_NAME, P.NAME_OF_POSITION positionName
+            FROM APPROVER_INFO AI
+            JOIN PERSONNEL_INFORMATION PI ON AI.APPROVER_NO = PI.EMP_NO
+            JOIN DEPARTMENT D ON D.DEPT_CODE = PI.DEPT_CODE
+            JOIN POSITION P ON P.POSITION_CODE = PI.POSITION_CODE
+            WHERE APPR_LINE_NO = #{apprLineNo}
+            ORDER BY AI.SEQ
+            """)
     List<ApproverVo> getApproverList(int apprLineNo);
 
 
@@ -54,119 +59,111 @@ public interface DocumentMapper {
     int writeDocumentFile(List<DocFileVo> vo);
 
     // 내가 작성한 결재 문서 목록 조회(카테고리, 양식, 기안자관련)
-    @Select("SELECT D.DOC_NO ,D.WRITER_NO ,D.STATUS ,D.TITLE ,D.CONTENT ,D.ENROLL_DATE,D.CREDIT_DATE,STATUS\n" +
-            "    ,D.URGENT ,T.TITLE AS templateTitle ,TC.NAME AS categoryName ,D.STATUS, DSL.DOC_STATUS_NAME statusName,PI.NAME AS writerName ,DEPT.PARTNAME AS deptName \n" +
-            "   ,P.NAME_OF_POSITION AS positionName ,DRL.REFERRER_NO \n" +
-            "    FROM DOCUMENT D JOIN DOC_STATUS_LIST DSL ON D.STATUS = DSL.DOC_STATUS_NO\n" +
-            "    JOIN DOC_TEMPLATE T ON D.TEMPLATE_NO = T.TEMPLATE_NO \n" +
-            "    JOIN DOC_TEMPLATE_CATEGORY TC ON T.CATEGORY_NO = TC.CATEGORY_NO \n" +
-            "    JOIN PERSONNEL_INFORMATION PI ON D.WRITER_NO = PI.EMP_NO \n" +
-            "    LEFT JOIN DOC_REFERENCE_LIST DRL ON DRL.REFERRER_NO = PI.EMP_NO \n" +
-            "    LEFT JOIN DEPARTMENT DEPT ON DEPT.DEPT_CODE = PI.DEPT_CODE \n" +
-            "    LEFT JOIN POSITION P ON P.POSITION_CODE = PI.POSITION_CODE \n" +
-            "    WHERE D.DEL_YN ='N' AND D.CREDIT_DATE IS NOT NULL AND D.WRITER_NO = #{loginUserNo}\n" +
-            "    ORDER BY D.CREDIT_DATE DESC")
+    @Select("""
+            SELECT D.DOC_NO ,D.WRITER_NO ,D.STATUS ,D.TITLE ,D.CONTENT ,D.ENROLL_DATE,D.CREDIT_DATE,STATUS ,D.URGENT ,T.TITLE AS templateTitle
+            ,TC.NAME AS categoryName ,D.STATUS, DSL.DOC_STATUS_NAME statusName,PI.NAME AS writerName ,DEPT.PARTNAME AS deptName
+            ,P.NAME_OF_POSITION AS positionName
+            FROM DOCUMENT D JOIN DOC_STATUS_LIST DSL ON D.STATUS = DSL.DOC_STATUS_NO
+            JOIN DOC_TEMPLATE T ON D.TEMPLATE_NO = T.TEMPLATE_NO
+            JOIN DOC_TEMPLATE_CATEGORY TC ON T.CATEGORY_NO = TC.CATEGORY_NO
+            JOIN PERSONNEL_INFORMATION PI ON D.WRITER_NO = PI.EMP_NO
+            LEFT JOIN DEPARTMENT DEPT ON DEPT.DEPT_CODE = PI.DEPT_CODE
+            LEFT JOIN POSITION P ON P.POSITION_CODE = PI.POSITION_CODE
+            WHERE D.DEL_YN ='N' AND D.CREDIT_DATE IS NOT NULL AND D.WRITER_NO = #{loginUserNo}
+            ORDER BY D.CREDIT_DATE DESC
+            """)
     List<DocumentVo> getDocumentList(String loginUserNo);
 
     // 결재선 목록 조회
-    @Select("SELECT AL.DOC_NO AS approvalDocNo \n" +
-            "    ,AL.SEQ ,AL.APPROVAL_DATE ,AL.APPROVER_CLASSIFICATION_NO ,PI.NAME AS writerName ,DEPT.PARTNAME AS dept \n" +
-            "    ,P.NAME_OF_POSITION AS positionName ,DRL.REFERRER_NO, AL.APPROVAL_STAGE, ASL.APPR_STAGE_NAME\n" +
-            "    FROM APPR_LINE AL JOIN APPR_STAGE_LIST ASL ON AL.APPROVAL_STAGE = ASL.APPR_STAGE_NO\n" +
-            "    JOIN PERSONNEL_INFORMATION PI ON AL.APPROVER_NO = PI.EMP_NO \n" +
-            "    LEFT JOIN DOC_REFERENCE_LIST DRL ON DRL.REFERRER_NO = PI.EMP_NO \n" +
-            "    LEFT JOIN DEPARTMENT DEPT ON DEPT.DEPT_CODE = AL.DEPT_CODE \n" +
-            "    LEFT JOIN POSITION P ON P.POSITION_CODE = AL.POSITION_CODE \n" +
-            "    WHERE AL.DOC_NO = #{docNo}" +
-            "    ORDER BY AL.SEQ")
+    @Select("""
+            SELECT AL.DOC_NO AS approvalDocNo
+            ,AL.SEQ ,AL.APPROVAL_DATE ,AL.APPROVER_CLASSIFICATION_NO ,PI.NAME AS writerName ,DEPT.PARTNAME AS dept
+            ,P.NAME_OF_POSITION AS positionName ,DRL.REFERRER_NO, AL.APPROVAL_STAGE, ASL.APPR_STAGE_NAME
+            FROM APPR_LINE AL JOIN APPR_STAGE_LIST ASL ON AL.APPROVAL_STAGE = ASL.APPR_STAGE_NO
+            JOIN PERSONNEL_INFORMATION PI ON AL.APPROVER_NO = PI.EMP_NO
+            LEFT JOIN DOC_REFERENCE_LIST DRL ON DRL.REFERRER_NO = PI.EMP_NO
+            LEFT JOIN DEPARTMENT DEPT ON DEPT.DEPT_CODE = AL.DEPT_CODE
+            LEFT JOIN POSITION P ON P.POSITION_CODE = AL.POSITION_CODE
+            WHERE AL.DOC_NO = #{docNo}" +
+            ORDER BY AL.SEQ
+            """)
     List<ApprovalLineVo> getApprovalLineList(int docNo);
 
     // 임시 보관함
     // 내가 작성한 결재 문서 목록 조회(카테고리, 양식, 기안자관련)
-//    @Select("SELECT D.DOC_NO ,D.WRITER_NO ,D.STATUS ,D.TITLE ,D.CONTENT ,D.ENROLL_DATE,D.CREDIT_DATE,STATUS\n" +
-//            "    ,D.URGENT ,T.TITLE AS templateTitle ,TC.NAME AS categoryName ,D.STATUS, DSL.DOC_STATUS_NAME statusName,PI.NAME AS writerName ,DEPT.PARTNAME AS deptName \n" +
-//            "   ,P.NAME_OF_POSITION AS positionName ,DRL.REFERRER_NO \n" +
-//            "    FROM DOCUMENT D JOIN DOC_STATUS_LIST DSL ON D.STATUS = DSL.DOC_STATUS_NO\n" +
-//            "    JOIN DOC_TEMPLATE T ON D.TEMPLATE_NO = T.TEMPLATE_NO \n" +
-//            "    JOIN DOC_TEMPLATE_CATEGORY TC ON T.CATEGORY_NO = TC.CATEGORY_NO \n" +
-//            "    JOIN PERSONNEL_INFORMATION PI ON D.WRITER_NO = PI.EMP_NO \n" +
-//            "    LEFT JOIN DOC_REFERENCE_LIST DRL ON DRL.REFERRER_NO = PI.EMP_NO \n" +
-//            "    LEFT JOIN DEPARTMENT DEPT ON DEPT.DEPT_CODE = PI.DEPT_CODE \n" +
-//            "    LEFT JOIN POSITION P ON P.POSITION_CODE = PI.POSITION_CODE \n" +
-//            "    WHERE D.DEL_YN ='N' AND D.CREDIT_DATE IS NULL AND D.WRITER_NO = #{loginUserNo}\n" +
-//            "    ORDER BY D.CREDIT_DATE DESC")
-//    List<DocumentVo> getDocumentList(String loginUserNo);
 
     // 내가 받은 결재
     @Select("""
             SELECT D.DOC_NO, D.WRITER_NO, D.STATUS, D.TITLE, D.CONTENT, D.ENROLL_DATE, D.CREDIT_DATE, D.STATUS,
                   D.URGENT, T.TITLE AS templateTitle, TC.NAME AS categoryName, D.STATUS, DSL.DOC_STATUS_NAME statusName,
-                  PI.NAME AS writerName, DEPT.PARTNAME AS deptName, P.NAME_OF_POSITION AS positionName, DRL.REFERRER_NO
+                  PI.NAME AS writerName, DEPT.PARTNAME AS deptName, P.NAME_OF_POSITION AS positionName
            FROM DOCUMENT D
            JOIN APPR_LINE A ON D.DOC_NO = A.DOC_NO
            JOIN DOC_STATUS_LIST DSL ON D.STATUS = DSL.DOC_STATUS_NO
            JOIN DOC_TEMPLATE T ON D.TEMPLATE_NO = T.TEMPLATE_NO
            JOIN DOC_TEMPLATE_CATEGORY TC ON T.CATEGORY_NO = TC.CATEGORY_NO
            JOIN PERSONNEL_INFORMATION PI ON D.WRITER_NO = PI.EMP_NO
-           LEFT JOIN DOC_REFERENCE_LIST DRL ON DRL.REFERRER_NO = PI.EMP_NO
            LEFT JOIN DEPARTMENT DEPT ON DEPT.DEPT_CODE = PI.DEPT_CODE
            LEFT JOIN POSITION P ON P.POSITION_CODE = PI.POSITION_CODE
            WHERE A.APPROVER_NO = #{loginUserNo}
-             AND D.CREDIT_DATE IS NOT NULL
              AND A.APPROVAL_STAGE = 1
-             AND D.STATUS = '2'
-             AND (
-                 A.SEQ <= 1 OR
-                 A.DOC_NO IN (
-                     SELECT AL.DOC_NO
-                     FROM APPR_LINE AL
-                     WHERE AL.DOC_NO = A.DOC_NO
-                       AND AL.SEQ = A.SEQ - 1
-                       AND AL.APPROVAL_STAGE = 3
-                 )
-             )
+             AND D.STATUS = 2
+              AND ( A.SEQ = 1 OR
+                   EXISTS (
+                       SELECT 1
+                           FROM APPR_LINE B
+                           WHERE A.DOC_NO = B.DOC_NO
+                               AND A.SEQ -1 = B.SEQ
+                               AND B.APPROVAL_STAGE = 3
+                       )
+                )
            ORDER BY D.CREDIT_DATE DESC
            """)
     List<DocumentVo> getSendDocumentList(String loginUserNo);
 
     // 상세보기
     // 결재 문서 조회(카테고리, 양식, 기안자관련)
-    @Select("SELECT D.DOC_NO, D.WRITER_NO, D.STATUS, D.TITLE, D.CONTENT, D.ENROLL_DATE, D.CREDIT_DATE, D.STATUS,\n" +
-            "D.URGENT, T.TITLE AS templateTitle, TC.NAME AS categoryName, DSL.DOC_STATUS_NAME AS statusName,\n" +
-            "PI.NAME AS writerName, DEPT.PARTNAME AS deptName, P.NAME_OF_POSITION AS positionName, DRL.REFERRER_NO\n" +
-            "FROM DOCUMENT D\n" +
-            "JOIN DOC_STATUS_LIST DSL ON D.STATUS = DSL.DOC_STATUS_NO\n" +
-            "JOIN DOC_TEMPLATE T ON D.TEMPLATE_NO = T.TEMPLATE_NO\n" +
-            "JOIN DOC_TEMPLATE_CATEGORY TC ON T.CATEGORY_NO = TC.CATEGORY_NO\n" +
-            "JOIN PERSONNEL_INFORMATION PI ON D.WRITER_NO = PI.EMP_NO\n" +
-            "LEFT JOIN DOC_REFERENCE_LIST DRL ON DRL.REFERRER_NO = PI.EMP_NO\n" +
-            "LEFT JOIN DEPARTMENT DEPT ON DEPT.DEPT_CODE = PI.DEPT_CODE\n" +
-            "LEFT JOIN POSITION P ON P.POSITION_CODE = PI.POSITION_CODE\n" +
-            "WHERE D.DOC_NO = #{docNo}")
+    @Select("""
+            SELECT D.DOC_NO, D.WRITER_NO, D.STATUS, D.TITLE, D.CONTENT, D.ENROLL_DATE, D.CREDIT_DATE, D.STATUS,
+            D.URGENT, T.TITLE AS templateTitle, TC.NAME AS categoryName, DSL.DOC_STATUS_NAME AS statusName,
+            PI.NAME AS writerName, DEPT.PARTNAME AS deptName, P.NAME_OF_POSITION AS positionName
+            FROM DOCUMENT D
+            JOIN DOC_STATUS_LIST DSL ON D.STATUS = DSL.DOC_STATUS_NO
+            JOIN DOC_TEMPLATE T ON D.TEMPLATE_NO = T.TEMPLATE_NO
+            JOIN DOC_TEMPLATE_CATEGORY TC ON T.CATEGORY_NO = TC.CATEGORY_NO
+            JOIN PERSONNEL_INFORMATION PI ON D.WRITER_NO = PI.EMP_NO
+            LEFT JOIN DEPARTMENT DEPT ON DEPT.DEPT_CODE = PI.DEPT_CODE
+            LEFT JOIN POSITION P ON P.POSITION_CODE = PI.POSITION_CODE
+            WHERE D.DOC_NO = #{docNo}
+            """)
     DocumentVo getDocumentByNo(int docNo);
 
     // 결재선 목록 조회
-    @Select("SELECT AL.DOC_NO AS approvalDocNo \n" +
-            "    ,AL.SEQ ,AL.APPROVAL_DATE ,AL.APPROVER_CLASSIFICATION_NO ,PI.NAME approverName ,DEPT.PARTNAME deptName \n" +
-            "    ,P.NAME_OF_POSITION positionName ,DRL.REFERRER_NO, AL.APPROVAL_STAGE, ASL.APPR_STAGE_NAME apprStageName\n" +
-            "    FROM APPR_LINE AL JOIN PERSONNEL_INFORMATION PI ON AL.APPROVER_NO = PI.EMP_NO\n " +
-            "    LEFT JOIN APPR_STAGE_LIST ASL ON AL.APPROVAL_STAGE = ASL.APPR_STAGE_NO\n" +
-            "    LEFT JOIN DOC_REFERENCE_LIST DRL ON DRL.REFERRER_NO = PI.EMP_NO \n" +
-            "    LEFT JOIN DEPARTMENT DEPT ON DEPT.DEPT_CODE = AL.DEPT_CODE \n" +
-            "    LEFT JOIN POSITION P ON P.POSITION_CODE = AL.POSITION_CODE \n" +
-            "    WHERE AL.DOC_NO = #{docNo} \n" +
-            "    ORDER BY AL.SEQ")
+    @Select("""
+            SELECT AL.DOC_NO AS approvalDocNo
+            ,AL.SEQ ,AL.APPROVAL_DATE ,AL.APPROVER_CLASSIFICATION_NO ,PI.NAME approverName ,DEPT.PARTNAME deptName
+            ,P.NAME_OF_POSITION positionName ,DRL.REFERRER_NO, AL.APPROVAL_STAGE, ASL.APPR_STAGE_NAME apprStageName
+            FROM APPR_LINE AL JOIN PERSONNEL_INFORMATION PI ON AL.APPROVER_NO = PI.EMP_NO
+            LEFT JOIN APPR_STAGE_LIST ASL ON AL.APPROVAL_STAGE = ASL.APPR_STAGE_NO
+            LEFT JOIN DOC_REFERENCE_LIST DRL ON DRL.REFERRER_NO = PI.EMP_NO
+            LEFT JOIN DEPARTMENT DEPT ON DEPT.DEPT_CODE = AL.DEPT_CODE
+            LEFT JOIN POSITION P ON P.POSITION_CODE = AL.POSITION_CODE
+            WHERE AL.DOC_NO = #{docNo}
+            ORDER BY AL.SEQ
+            """)
     List<ApproverVo> getApprovalLineByNo(int docNo);
 
     // 참조인 목록 조회
-    @Select("SELECT DL.DOC_NO,PI.NAME AS writerName ,DEPT.PARTNAME AS deptName \n" +
-            "   ,P.NAME_OF_POSITION AS positionName ,DRL.REFERRER_NO references\n" +
-            "    FROM DOC_REFERENCE_LIST DL\n" +
-            "    JOIN PERSONNEL_INFORMATION PI ON DL.REFERRER_NO = PI.EMP_NO \n" +
-            "    LEFT JOIN DOC_REFERENCE_LIST DRL ON DRL.REFERRER_NO = PI.EMP_NO \n" +
-            "    LEFT JOIN DEPARTMENT DEPT ON DEPT.DEPT_CODE = PI.DEPT_CODE \n" +
-            "    LEFT JOIN POSITION P ON P.POSITION_CODE = PI.POSITION_CODE \n" +
-            "   WHERE DL.DOC_NO = #{docNo}")
+    @Select("""
+            SELECT DL.DOC_NO,PI.NAME AS writerName ,DEPT.PARTNAME AS deptName
+            ,P.NAME_OF_POSITION AS positionName ,DRL.REFERRER_NO references
+            FROM DOC_REFERENCE_LIST DL
+            JOIN PERSONNEL_INFORMATION PI ON DL.REFERRER_NO = PI.EMP_NO
+            LEFT JOIN DOC_REFERENCE_LIST DRL ON DRL.REFERRER_NO = PI.EMP_NO
+            LEFT JOIN DEPARTMENT DEPT ON DEPT.DEPT_CODE = PI.DEPT_CODE
+            LEFT JOIN POSITION P ON P.POSITION_CODE = PI.POSITION_CODE
+            WHERE DL.DOC_NO = #{docNo}
+            """)
     List<ReferencerVo> getReferencerByNo(int docNo);
 
     // 파일 목록 조회
@@ -175,15 +172,15 @@ public interface DocumentMapper {
 
     // 결재 기안 철회(아무도 결재승인 안했을 경우 가능)
     @Update("""
-            UPDATE DOCUMENT 
-            SET DEL_YN = 'Y' 
-            WHERE DOC_NO = #{docNo} 
+            UPDATE DOCUMENT
+            SET DEL_YN = 'Y'
+            WHERE DOC_NO = #{docNo}
             AND WRITER_NO = #{loginUserNo}
-            AND DOC_NO NOT 
+            AND DOC_NO NOT
                 IN (
-                SELECT DOC_NO 
-                FROM APPR_LINE 
-                WHERE DOC_NO = #{docNo} 
+                SELECT DOC_NO
+                FROM APPR_LINE
+                WHERE DOC_NO = #{docNo}
                 AND APPROVAL_STAGE != 1
             )
             """)
