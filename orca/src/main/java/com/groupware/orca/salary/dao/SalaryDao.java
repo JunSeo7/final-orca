d1be5e647d07c463d31ac34bda71351d29acf88c
DESKTOP-4D7E96M\user1 <chlgp5717@naver.com>
2024-07-05 20:25:12 +0900

    [edit] salary 수정

--- orca/src/main/java/com/groupware/orca/salary/dao/SalaryDao.java
+++ orca/src/main/java/com/groupware/orca/salary/dao/SalaryDao.java
@@ -1,45 +1,62 @@
-//package com.groupware.orca.salary.dao;
-//
-//import com.groupware.orca.salary.mapper.SalaryMapper;
-//import com.groupware.orca.salary.vo.RatesVo;
-//import com.groupware.orca.salary.vo.SalaryVo;
-//import com.groupware.orca.user.vo.UserVo;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//
-//@Repository
-//@RequiredArgsConstructor
-//public class SalaryDao {
-//
-//    private final SalaryMapper mapper;
-//
-//    public double salaryWrite(UserVo vo, SalaryVo svo, RatesVo rvo, double holidayTime, double overTime, double person, double position) {
-//        System.out.println("SalaryDao.salaryWrite");
-//        return mapper.salaryWrite(vo,svo,rvo,holidayTime,overTime,person,position);
-//
-//    }
-//
-//    public int ratesWrite(RatesVo vo) {
-//        return mapper.ratesWrite(vo);
-//    }
-//
-//    public List<SalaryVo> getSalaryList() {
-//        System.out.println("SalaryDao.getSalaryList");
-//        return mapper.getSalaryList();
-//    }
-//
-//    public SalaryVo getSalaryVo() {
-//        return mapper.getSalaryVo();
-//    }
-//
-//
-//    public RatesVo getRatesVo() {
-//        return mapper.getRatesVo();
-//    }
-//
-//    public UserVo getUserVo() {
-//        return mapper.getUserVo();
-//    }
-//}
+package com.groupware.orca.salary.dao;
+
+import com.groupware.orca.salary.mapper.SalaryMapper;
+import com.groupware.orca.salary.vo.PayrollVo;
+import com.groupware.orca.salary.vo.RatesVo;
+import com.groupware.orca.salary.vo.SalaryVo;
+import com.groupware.orca.user.vo.UserVo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class SalaryDao {
+
+    private final SalaryMapper mapper;
+
+    public int salaryWrite(UserVo vo, RatesVo rvo, double holidayTime, double overTime, double person, double position) {
+        System.out.println("SalaryDao.salaryWrite");
+        return mapper.salaryWrite(vo,rvo,holidayTime,overTime,person,position);
+
+    }
+
+    public int ratesWrite(RatesVo vo) {
+        return mapper.ratesWrite(vo);
+    }
+
+    public List<SalaryVo> getSalaryList() {
+        System.out.println("SalaryDao.getSalaryList");
+        return mapper.getSalaryList();
+    }
+
+    public SalaryVo getSalaryVo(String empNo) {
+        System.out.println("empNo-dao-svo: " + empNo);
+        return mapper.getSalaryVo(empNo);
+    }
+
+
+    public RatesVo getRatesVo() {
+        System.out.println("Dao-rvo called");
+        return mapper.getRatesVo();
+    }
+
+    public UserVo getUserVo(String empNo) {
+        return mapper.getUserVo(empNo);
+    }
+
+    //TODO swy 삭제
+    public RatesVo getRates() {
+        return mapper.getRates();
+    }
+
+
+    public UserVo getPersonalInformation(String empNo) {
+        return mapper.getPersonalInformation(empNo);
+    }
+
+    public int insertPayroll(PayrollVo vo) {
+        return mapper.insertPayroll(vo);
+    }
+}
--- orca/src/main/java/com/groupware/orca/salary/vo/RatesVo.java
+++ orca/src/main/java/com/groupware/orca/salary/vo/RatesVo.java
@@ -7,9 +7,9 @@ public class RatesVo {
 
     private String baseYear;            //기준년도 (4대보험 요율 해당 연도)
     private double pensionPercentage;      //국민연금
-    private double healthPercentage;       //건강보험
+    private double healthInsurancePercentage; //HEALTH_INSURANCE_PERCENTAGE;       //건강보험
     private double longCarePercentage;               //장기요양보험
-    private double employmentPercentage;   //고용보험 (실업급여)
+    private double employmentInsurancePercentage; //EMPLOYMENT_INSURANCE_PERCENTAGE;   //고용보험 (실업급여)
     private double localIncomeTaxPercentage; //지방 소득세
 }
 
