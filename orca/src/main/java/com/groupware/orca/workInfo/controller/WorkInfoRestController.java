ae2877b7827b5e6331a7265bd71b9edd19eeb129
DESKTOP-PNTRVU6\user1 <wnstj1478@gmail.com>
2024-07-19 16:28:55 +0900

    [add] 전체 사원 근무 정보 조회 페이지 생성 [edit] 사원 근무 정보 조회 기능 코드 수정

--- orca/src/main/java/com/groupware/orca/workInfo/controller/WorkInfoRestController.java
+++ orca/src/main/java/com/groupware/orca/workInfo/controller/WorkInfoRestController.java
@@ -1,5 +1,6 @@
 package com.groupware.orca.workInfo.controller;
 
+import com.groupware.orca.common.vo.PageVo;
 import com.groupware.orca.user.vo.UserVo;
 import com.groupware.orca.workInfo.service.WorkInfoService;
 import com.groupware.orca.workInfo.vo.WorkInfoVo;
@@ -24,9 +25,15 @@ public class WorkInfoRestController {
 
     // 모든 사원 근무 정보 리스트
     @GetMapping("allList")
-    public List<WorkInfoVo> allWorkList(){
-        List<WorkInfoVo> Wvo = service.allWorkList();
-        return Wvo;
+    public List<WorkInfoVo> getAllWorkInfo(@RequestParam int page, @RequestParam int recordSize) {
+        PageVo pageVo = new PageVo();
+        pageVo.setPage(page);
+        pageVo.setRecordSize(recordSize);
+
+        // 예시: 데이터베이스에서 페이지에 해당하는 데이터를 가져오는 로직 추가
+        List<WorkInfoVo> workInfoList = service.getAllWorkInfo(pageVo);
+
+        return workInfoList;
     }
 
     // 개인 근무정보 리스트
